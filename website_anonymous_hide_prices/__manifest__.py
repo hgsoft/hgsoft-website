126689bd28b9dc3dad2e11e06eaf38da0a6328bc
Alexsandro Haag <alex@hgsoft.com.br>
2017-02-08 10:41:41 -0200

    Iniciada migração do módulo para versão 10.0

--- website_anonymous_hide_prices/__manifest__.py
+++ website_anonymous_hide_prices/__manifest__.py
@@ -11,12 +11,13 @@
 
     'author': "Michael Hucke, extended by HGSOFT",
     'website': "http://www.hgsoft.com.br",
+    'contributors': ['Alexsandro Haag <alex@hgsoft.com.br>'],
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'website',
-    'version': '0.1',
+    'version': '10.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
