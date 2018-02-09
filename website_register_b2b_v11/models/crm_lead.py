8712ad5c1768f4e8b9e2a15e6790dd8cd1907ade
Ewerton Rover <ewerton@hgsoft.com.br>
2018-02-09 16:45:28 -0200

    Adicionados comentários referente aos parametros do metódo write()

--- website_register_b2b_v11/models/crm_lead.py
+++ website_register_b2b_v11/models/crm_lead.py
@@ -111,13 +111,28 @@ class Lead2OpportunityMassConvert(models.TransientModel):
     
     
     
+    """
+    
+    #####
+    
+    #Params: res.groups.write()
+    
+    #####
+    
+    0, 0, { values }) link to a new record that needs to be created with the given values dictionary
 
-    #####
-    """
-    def set_commission_group(self, user_id):
-        commission_group = self.env.ref('your_module_name.salesmen_commission_group')
-        commission_group.write({'users': [(4, user_id)]})
-    """
+    (1, ID, { values }) update the linked record with id = ID (write values on it)
+
+    (2, ID) remove and delete the linked record with id = ID (calls unlink on ID, that will delete the object completely, and the link to it as well)
+
+    (3, ID) cut the link to the linked record with id = ID (delete the relationship between the two objects but does not delete the target object itself)
+
+    (4, ID) link to existing record with id = ID (adds a relationship)
+
+    (5) unlink all (like using (3,ID) for all linked records)
+
+    (6, 0, [IDs]) replace the list of linked IDs (like using (5) then (4,ID) for each ID in the list of IDs)
+    
     #####
     
-    
+    """
